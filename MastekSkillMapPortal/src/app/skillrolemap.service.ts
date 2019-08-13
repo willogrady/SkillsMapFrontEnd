939a688d3777cfad801822f350634891090c8481
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-08-13 09:04:52 +0100

    commit to pull

--- MastekSkillMapPortal/src/app/skillrolemap.service.ts
+++ MastekSkillMapPortal/src/app/skillrolemap.service.ts
@@ -15,10 +15,10 @@ export class SkillrolemapService {
     this.rootURL = "http://localhost:9901/role_skill_map";
    }
 
-   getSfiaSkillviaId(skillcode: string): Observable<SfiaSkill> {
-     return this.httpsvc.get<SfiaSkill>(this.rootURL +
-    "/sfia_skill?skillcode" + skillcode);
-   }
+  //  getSfiaSkillviaId(skillcode: string): Observable<SfiaSkill> {
+  //    return this.httpsvc.get<SfiaSkill>(this.rootURL +
+  //   "/sfia_skill?skillcode" + skillcode);
+  //  }
 
    getRoleSkillMapviaSkillCode(skillcode: string): Observable<RoleSkillMap[]> {
      return this.httpsvc.get<RoleSkillMap[]>(this.rootURL +
