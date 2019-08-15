109ffdc39f1b6bfb4f18cfa97e276b0e2c9f5057
jordangowdy11 <jordan.gowdy@mastek.com>
2019-08-15 14:09:11 +0100

    logout and login with big azzy ad

--- MastekSkillMapPortal/src/app/app.component.ts
+++ MastekSkillMapPortal/src/app/app.component.ts
@@ -8,17 +8,19 @@ import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
 })
 export class AppComponent {
   title = 'MastekSkillMapPortal';
+  user: MsAdalAngular6Service[];
 
   constructor(private adalSvc: MsAdalAngular6Service) {
     console.log(this.adalSvc.userInfo);
     var token = this.adalSvc.acquireToken('https://graph.microsoft.com').subscribe((token: string) => {
     console.log(token);
 });
-
     
   }
   
-
+  userLogout() {
+    this.adalSvc.logout();
+  }
 
 
 
