f09cd211e4acfc6d9eb11fb26c696a0f9f45979c
jordangowdy11 <jordan.gowdy@mastek.com>
2019-09-03 09:46:56 +0100

    edit on company skill

--- MastekSkillMapPortal/src/app/company-skills/company-skills.component.ts
+++ MastekSkillMapPortal/src/app/company-skills/company-skills.component.ts
@@ -10,10 +10,25 @@ import { Companyskills } from '../companyskills';
 export class CompanySkillsComponent implements OnInit {
 
   companySkills: Companyskills[];
+  isEditable: boolean;
 
   constructor(private compSvc: CompanySkillsService) {
-    this.companySkills = [];
+    this.isEditable = false;
    }
+
+
+   pressedEdit() {
+    this.isEditable = !this.isEditable;
+   }
+
+
+   updateSkills(company_skill_id: string, company_skill: string, version_id: string,
+    company_skill_description: string, company_skill_category: string, company_skill_sub_category: string) {
+     this.compSvc.updateCompanySkillDetails(company_skill_id, company_skill, version_id, company_skill_description, company_skill_category, company_skill_sub_category).subscribe(
+       res =>(this.companySkills = res)
+     )
+   }
+
 
   ngOnInit() {
     this.compSvc.getCompanySkills().subscribe(
